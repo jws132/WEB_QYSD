d781ea2d9bfe161e4b9aa82c365bff43b7bd0a05
jws132 <jiangwenshu@qiyestore.com>
2016-01-16 10:07:21 +0800

    企用下载页面"

---
+++ qyDownload/Gruntfile.js
@@ -0,0 +1,160 @@
+﻿module.exports=function(grunt){
+    require('load-grunt-tasks')(grunt);
+	require('time-grunt')(grunt);
+	
+    grunt.initConfig({
+	    pack:grunt.file.readJSON('package.json'),
+		//拷贝目录/文件
+		copy:{
+			html:{
+				files:[{
+                  expand:true,
+                  src:['index.html','!node_modules','!*.js'],
+                  dest:'public'
+				}]
+			},
+			images:{
+				files:[{
+				  	expand:true,
+				  	src:'style/images/*',
+				  	dest:'public/'
+				}]
+			},
+			js:{
+				files:[{
+				  	expand:true,
+				  	cwd:'style/',
+				  	src:'*.js',
+				  	dest:'public/style/'
+				}]
+			},
+			css:{
+				files:[{
+				  	expand:true,
+				  	cwd:'style/',
+				  	src:'*.css',
+				  	dest:'public/style/'
+				}]
+			}
+		},
+		//合并文件
+		/**concat:{
+			css:{
+				src:'style/*.css',
+				dest:'public/style/style.min.css'
+			},
+			js:{
+				src:'style/*.js',
+				dest:'public/js/app.min.js'
+			}
+
+		},**/
+		//图片压缩
+		imagemin: {
+		    dynamic: {
+		        options: {
+		            optimizationLevel: 3 // png图片优化水平，3是默认值，取值区间0-7
+		        },
+		        files: [
+		            {
+		                expand: true, // 开启动态扩展
+		                cwd: "public/images/", // 当前工作路径
+		                src: ["*.{png,jpg,gif}"], // 要出处理的文件格式(images下的所有png,jpg,gif)
+		                dest: "public/style/images/" // 输出目录(直接覆盖原图)
+		            }
+		        ]
+		    }
+		},
+		//合并压缩js
+		uglify:{
+		   bulid:{
+		   	files:[{
+                expand: true, // 开启动态扩展
+                cwd: "public/style/", // 当前工作路径
+                src: '*.js', // 要出处理的文件格式(images下的所有png,jpg,gif)
+                dest: "public/style/" // 输出目录(直接覆盖原图)
+		   	}]
+		   }
+		},
+		//压缩css
+		cssmin:{
+			css:{	
+				files:{
+					'public/style/style.css':'public/style/style.css',
+					'public/style/jquery.fullPage.css':'public/style/jquery.fullPage.css'
+				}
+			}
+		},
+		//清楚
+		clean:{
+		    src:['public/*','public/*.*'],
+		},
+		 // 处理html中css、js 引入合并问题
+	    usemin: {
+            css:{
+                files:{
+                    src:['public/style/*.css']
+                }
+            },
+            js:['public/style/*.js'],
+            html:['public/*.html'],
+	    },
+	    //添加时间戳
+	    rev: {
+		    options: {
+		      encoding: 'utf8',
+		      algorithm: 'md5',
+		      length: 4
+		    },
+		    assets: {
+		      files: [{
+		        src: [
+		          'public/style/images/*.{jpg,jpeg,gif,png}',
+		          'public/style/*.js',
+		          'public/style/*.css'
+		        ]
+		      }]
+		    }
+		},
+        //压缩html页面
+	    htmlmin:{
+		    dist: {
+		        options: {
+		          removeComments: false,
+		          collapseWhitespace: true
+		        },
+		        files: {
+		          'public/index.html': 'public/index.html'
+		        }
+		    }
+	    },
+	    watch: {
+            files: [  //下面文件的改变就会实时刷新网页
+                'public/*.html',
+                'public/style/{,*/}*.css',
+                'public/style/{,*/}*.js',
+                'public/images/{,*/}*.{png,jpg,gif}'
+            ]
+        },
+	    //静态文件服务器
+	    connect:{
+			 options: {
+			        port: 9000,
+			        hostname: '*', //默认就是这个值，可配置为本机某个 IP，localhost 或域名
+			        livereload: 35729  //声明给 watch 监听的端口
+			  },
+			  server: {
+			        options: {
+			          open: true, //自动打开网页 http://
+			          base:'public'  //主目录
+			        }
+			  }
+	    }
+
+	})
+
+
+    // Default task(s)
+    grunt.registerTask('mian', ['clean','copy','uglify','cssmin','rev','usemin','htmlmin']);
+    grunt.registerTask('default', ['mian','connect:server','watch']);
+};
---
+++ qyDownload/style/app.js
@@ -0,0 +1,115 @@
+﻿$(function(){
+	$('#qyDownload').fullpage({
+	'verticalCentered': false,
+	'sectionsColor': ['#14a9e1','#0b9ed4','#0b9ed4','#0b9ed4','#0b9ed4'],
+	'anchors': ['page1', 'page2', 'page3','page4','page5'],
+	afterRender: function(){
+        $('.inner .p1').each(function(){
+			var $arr = $(this).attr('rel').split(',');
+			$(this).animate({
+					left: $arr[0] + 'px',
+					top: $arr[1] + 'px'
+					
+			}, 500);
+		}); 
+	},
+	afterLoad: function(anchorLink, index){
+		if(index==1 || index==2 || index==3 || index==4 || index==5){
+			$('.inner .p'+index).each(function(){
+				var $arr = $(this).attr('rel').split(',');
+				$(this).animate({
+						left: $arr[0] + 'px',
+						top: $arr[1] + 'px'
+						
+				}, 500);
+			});
+			if(index==2){
+				$('.p2-home').animate({ bottom: '20px'});
+				$('.p2-ren').animate({ right: '0px'},1000);
+			}
+			if(index==3){
+				$('.section3 .p-icon').each(function(){
+						var $arr = $(this).attr('rel').split(',');
+						$(this).animate({
+								left: $arr[0] + 'px',
+								bottom: $arr[1] + 'px',
+								opacity:1,
+								
+						}, 1000);
+					});
+		     	$('.p3-d').animate({ bottom: '0px'},300);
+			    $('.p3-ren').animate({ right: '120px'},500);
+			    $('.section4 .p-icon').animate({ opacity: '0'},500);
+			}
+			if(index==4){
+				$('.p4-bg').animate({ opacity: '1'},500);  
+                $('.p4-ren').animate({ opacity: '1'},500);  
+				$('.p4-x').animate({ width: '512px'},500);  
+				$('.section4 .p-icon').each(function(){
+					$(this).animate({ opacity: '1'},500);  
+				})		 
+			}
+			if(index==5){
+			     $('.p5-y').animate({ width: '647px'},300);
+				 $('.p5-icon-1').animate({bottom:'340px',left:'10px',opacity:'1'},1000);
+				 $('.section4 .p-icon').animate({ opacity: '0'},500);
+				 setTimeout(function () {
+				      $('.section5 .p-yimg').each(function(){
+					      $(this).animate({opacity:'1'}, 500);
+					  });
+				 },1000)
+				  setTimeout(function () {
+				      $('.p5-ren').animate({ opacity: '1'},500);  
+				 },1000)
+			}
+		}
+
+	},
+	'onLeave': function(index, direction){
+		if(index==1 || index==2 || index==3 || index==4 || index==5){
+			$('.inner .p'+index).each(function(){
+				var $arr = $(this).attr('rel').split(',');
+				$(this).animate({
+						left: $arr[2] + 'px',
+						top: $arr[3] + 'px'
+						
+				}, 500);
+			});
+			if(index==2){
+			    $('.p2-home').animate({ bottom: '-279px'});
+			    $('.p2-ren').animate({ right: '-550px'},1000);
+			}
+			if(index==3){
+				$('.section3 .p-icon').each(function(){
+						var $arr = $(this).attr('rel').split(',');
+						$(this).animate({
+								left: $arr[2] + 'px',
+								bottom: $arr[3] + 'px'
+								
+						}, 500);
+					});
+			    $('.p3-d').animate({ bottom: '-485px'},1000);
+			    $('.p3-ren').animate({ right: '-550px'},500);
+			}
+			if(index==4){
+                  $('.p4-bg').animate({ opacity: '0'},500);  
+	              $('.p4-ren').animate({ opacity: '0'},500);  
+				  $('.p4-x').animate({ width: '0px'},500); 
+					$('.section4 .p-icon').each(function(){
+						$(this).animate({ opacity: '1'},500);  
+					})	
+			}
+            if(index==5){
+			     $('.p5-y').animate({ width: '0px'},300);
+				 $('.p5-icon-1').animate({bottom:'340px',left:'10px',opacity:'0'},1000);
+                 $('.section5 .p-yimg').each(function(){
+					$(this).animate({opacity:'0'}, 500);
+				  });
+				  $('.p5-ren').animate({ opacity: '0'},500);  
+			}
+		}	
+		
+	}
+
+	});
+});
